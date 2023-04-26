b3b718a2f5c14dc8b0579234b5b1b3e12ffd083c
Al Fiana <89647942+fian3021@users.noreply.github.com>
2023-04-26 23:09:45 +0800

    Update Petugas_Vaksin.java

--- PA/src/PA/Petugas_Vaksin.java
+++ PA/src/PA/Petugas_Vaksin.java
@@ -202,8 +202,8 @@ public class Petugas_Vaksin extends User implements Informasi_Jadwal_Lokasi{
     public void menu() throws IOException {
         while(true){
             System.out.println("==================================================================================");
-            System.out.println("|+-+-+-+-+-+-+-+-+-              MENU ADMINISTATOR             -+-+-+-+-+-+-+-+-+|"); 
-            System.out.println("|+-+-+-+-+-+-       INFORMASI JADWAL & LOKASI VAKSINASI COVID-19     -+-+-+-+-+-+|");
+            System.out.println("|+-+-+-+-+-+-+-+-+-          MENU PETUGAS VAKSINASI            -+-+-+-+-+-+-+-+-+|");
+            System.out.println("|+-+-+-+-+-+-           PENDATAAN PESERTA VAKSINASI COVID-19         -+-+-+-+-+-+|");
             System.out.println("==================================================================================");
             System.out.println("|            1. Tambah Informasi Jadwal & Lokasi Vaksinasi   [CREATE]            |");
             System.out.println("|            2. Tampil Informasi Jadwal & Lokasi Vaksinasi   [READ]              |");
